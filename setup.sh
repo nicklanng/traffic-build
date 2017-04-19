8da81732b80220bf1c8a6a36055d0b9b29911409
Nick Lanng <rinse@rinseworld.com>
2017-04-19 16:50:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ pip3 install requests-2.13.0-py2.py3-none-any.whl
 
 chmod +x /home/$USER/traffic-build/launcher.sh
 
-(crontab -l ; echo "@reboot ./home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1")| crontab -
+(crontab -l ; echo "@reboot ./home/$USER/traffic-build/launcher.sh > /home/$USER/traffic-build/log 2>&1")| crontab -
 
-./home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1 &
+./home/$USER/traffic-build/launcher.sh > /home/$USER/traffic-build/log 2>&1 &
