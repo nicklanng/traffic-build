41231cfbf969e33986afa2bc5da5622c967c5435
Nick Lanng <rinse@rinseworld.com>
2017-04-19 16:46:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ git clone https://github.com/nicklanng/traffic-build
 cd traffic-build
 pip3 install requests-2.13.0-py2.py3-none-any.whl
 
-(crontab -l ; echo "@reboot sh /home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1")| crontab -
+(crontab -l ; echo "@reboot ./home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1")| crontab -
 
-sh /home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1 &
+./home/$USER/traffic-build/launcher.sh >/var/logs/traffic-build 2>&1 &
