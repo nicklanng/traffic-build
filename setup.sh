2cc7a5f49b7c5585286bab617d1c3fb21ee72cd2
Nick Lanng <rinse@rinseworld.com>
2017-04-24 14:16:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ sudo pip3 install requests-2.13.0-py2.py3-none-any.whl
 sudo chmod 755 /usr/local/bin/traffic-build/main.py
 
 sudo cp trafficbuild.sh /etc/init.d
-sudo chmod 755 /usr/local/bin/traffic-build/trafficbuild.sh
+sudo chmod 755 /etc/init.d/trafficbuild.sh
 sudo update-rc.d trafficbuild.sh defaults
 sudo /etc/init.d/trafficbuild.sh start
