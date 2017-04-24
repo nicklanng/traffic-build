82308278e921ecd393fb02c6c086decc4edfd551
Nick Lanng <rinse@rinseworld.com>
2017-04-24 14:08:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ pip3 install requests-2.13.0-py2.py3-none-any.whl
 
 chmod +x /usr/local/bin/traffic-build/main.py
 
-cp trafficlights.sh /etc/init.d
+cp trafficbuild.sh /etc/init.d
 chmod +x /usr/local/bin/traffic-build/trafficbuild.sh
 update-rc.d trafficbuild.sh defaults
 /etc/init.d/trafficbuild.sh start
