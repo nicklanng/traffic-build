0979e66f6795497d20cfea072923634d3b0e0511
Nick Lanng <rinse@rinseworld.com>
2017-04-24 14:14:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 if [ $(id -u) -ne 0 ]; then echo "Please run as root"; exit; fi
 
 cd /usr/local/bin
-git clone https://github.com/nicklanng/traffic-build
+sudo git clone https://github.com/nicklanng/traffic-build
 cd traffic-build
-pip3 install requests-2.13.0-py2.py3-none-any.whl
+sudo pip3 install requests-2.13.0-py2.py3-none-any.whl
 
-chmod +x /usr/local/bin/traffic-build/main.py
+sudo chmod 755 /usr/local/bin/traffic-build/main.py
 
-cp trafficbuild.sh /etc/init.d
-chmod +x /usr/local/bin/traffic-build/trafficbuild.sh
-update-rc.d trafficbuild.sh defaults
-/etc/init.d/trafficbuild.sh start
+sudo cp trafficbuild.sh /etc/init.d
+sudo chmod 755 /usr/local/bin/traffic-build/trafficbuild.sh
+sudo update-rc.d trafficbuild.sh defaults
+sudo /etc/init.d/trafficbuild.sh start
